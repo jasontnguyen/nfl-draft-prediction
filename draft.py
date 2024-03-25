39fe790bdfdbff26b59d5c08f3d598dc35bd951a
Jason Nguyen <jasontnguyen39@gmail.com>
2024-03-25 11:14:47 -0700

    added libraries

---
+++ draft.py
@@ -0,0 +1,17 @@
+import os
+import urllib
+
+# Numpy and Pandas libraries for storing and manipulating data
+import numpy as np
+import pandas as pd
+
+# Seaborn and Matplotlib for plotting
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# Scikit-Learn Descision Tree, Logistic Regression and helper function
+from sklearn import tree
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+
+RANDOM_SEED = 10
